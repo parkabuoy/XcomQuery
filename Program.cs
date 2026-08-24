8378307512e4cfb4116423f73d95a3efe925bdf4
parka <263944257+parkabuoy@users.noreply.github.com>
2026-08-24 18:08:37 -0500

    Add stats

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ string outputPath = "..\\..\\..\\txt\\output.txt";
 
 //----------------------------------------------------------------------------- parsing
 
-// build datatable out of csv file (directly swf's id reference sheets)
+// build datatable out of csv file (directly copied from swf's id reference sheets)
 DataTable perkRef = ConvertCSVtoDataTable("..\\..\\..\\csv\\Long War ID reference - Perks.csv");
 
 // parse save json into a class with newtonsoft.jsonconvert
@@ -47,6 +47,7 @@ foreach (CheckpointTable entity in save.checkpoints[0].checkpoint_table.Where(x 
       {
         id = (long)soldierProp.properties.Where(x => x.name == "iID").First().value,
         perksTaken = [],
+        stats = new(),
         // use helpers to make this a little cleaner
         lName = StringProp(soldierProp, "strLastName"),
         nName = StringProp(soldierProp, "strNickName"),
@@ -83,6 +84,33 @@ foreach (CheckpointTable entity in save.checkpoints[0].checkpoint_table.Where(x 
         }
       }
 
+      int[] aStats = [];
+      if (charProp.properties.Any(x => x.name == "aStats"))
+      {
+        aStats = [.. charProp.properties.First(x => x.name == "aStats").int_values];
+        for (int i = 0; i < aStats.Length; i++)
+        {
+          switch (i)
+          {
+            case 0:
+              thisSoldier.stats.HP = aStats[i];
+              break;
+            case 1:
+              thisSoldier.stats.Aim = aStats[i];
+              break;
+            case 2:
+              thisSoldier.stats.Defense = aStats[i];
+              break;
+            case 3:
+              thisSoldier.stats.Mobility = aStats[i];
+              break;
+            case 7:
+              thisSoldier.stats.Will = aStats[i];
+              break;
+          }
+        }
+      }
+
       // add the soldier to the roster
       roster.Add(thisSoldier);
     }
@@ -97,7 +125,7 @@ string indicator = "●";
 for (int i = 0; i < perkRef.Rows.Count; i++) perkNames += $"\t{perkRef.Rows[i]["Name"]}";
 
 // write header row
-Show($"id\tlName\tnName\trank\tstatus{perkNames}");
+Show($"id\tlName\tnName\trank\tstatus\tMOB\tHP\tDEF\tWILL\tAIM{perkNames}");
 
 // build soldier lines - iterate through roster
 foreach (Soldier thisSoldier in roster.Where(x => x.status != "Dead" && !string.IsNullOrEmpty(x.lName)))
@@ -107,7 +135,7 @@ foreach (Soldier thisSoldier in roster.Where(x => x.status != "Dead" && !string.
   // iterate through perk ref list and set this soldier's flag for each perk
   for (int i = 0; i < perkRef.Rows.Count; i++)
   {
-    // add tab to perkFlags string for this position
+    // add tab for this position regardless
     perkFlags += "\t";
 
     // if they have this perk (if any of their perks taken match this perk name), add the indicator string
@@ -115,7 +143,9 @@ foreach (Soldier thisSoldier in roster.Where(x => x.status != "Dead" && !string.
   }
 
   // write the line for this soldier
-  Show($"{thisSoldier.id}\t{thisSoldier.lName}\t{thisSoldier.nName}\t{thisSoldier.rank}\t{thisSoldier.status}{perkFlags}");
+  Show($"{thisSoldier.id}\t{thisSoldier.lName}\t{thisSoldier.nName}\t{thisSoldier.rank}\t{thisSoldier.status}" +
+    $"\t{thisSoldier.stats.Mobility}\t{thisSoldier.stats.HP}\t{thisSoldier.stats.Defense}\t{thisSoldier.stats.Will}\t{thisSoldier.stats.Aim}" +
+    $"{perkFlags}");
 }
 
 Console.WriteLine();
@@ -180,6 +210,16 @@ public class Soldier
   public long xp { get; set; }
   public string status { get; set; }
   public List<Perk> perksTaken { get; set; }
+  public SoldierStats stats {get;set;}
+}
+
+public class SoldierStats
+{
+    public long Mobility { get; set; }
+    public long Defense { get; set; }
+    public long Will { get; set; }
+    public long HP { get; set; }
+    public long Aim { get; set; }
 }
 
 public class Perk
